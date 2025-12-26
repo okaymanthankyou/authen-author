08627c6de8d47a2d515464962c56136e25458eee
Mahesh <okaymanthankyou@gmail.com>
2025-12-26 14:44:52 +0530

    Add Register API Backend

---
+++ backend/accounts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .serializers import UserRegistrationSerializer
+from rest_framework.permissions import AllowAny
+
+# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [AllowAny]
